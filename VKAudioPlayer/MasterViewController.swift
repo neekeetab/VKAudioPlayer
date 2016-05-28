bc77509e5bc61c2ad84a396eebe4726017592f9f
Nikita Belousov <nikita@nikitabs-macbook-pro.local>
2016-05-29 02:07:21 +0300

    more refactoring

--- VKAudioPlayer/MasterViewController.swift
+++ VKAudioPlayer/MasterViewController.swift
@@ -13,14 +13,17 @@ class MasterViewController: UIViewController, VKSdkDelegate, VKSdkUIDelegate {
 
     var tableViewController: TableViewController!
     var audioPlayerViewController: AudioPlayerViewController!
+    let indicatorView = UIActivityIndicatorView()
     
     // MARK: - IB
     @IBAction func searchButtonPressed(sender: AnyObject) {
         tableViewController.search(self)
     }
+    
     @IBAction func settingsButtonPressed(sender: AnyObject) {
     }
     
+    // MARK: - VK delegate
     func vkSdkUserAuthorizationFailed() {
         print("Authorization failed")
         // TODO: handle appropriately
@@ -45,7 +48,38 @@ class MasterViewController: UIViewController, VKSdkDelegate, VKSdkUIDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let sdkInstance = VKSdk.initializeWithAppId(appID)
+        sdkInstance.uiDelegate = self
+        sdkInstance.registerDelegate(self)
+        
+        //        VKSdk.forceLogout()
+        //        return
+        
+        indicatorView.center = view.center
+        indicatorView.activityIndicatorViewStyle = .Gray
+        indicatorView.hidesWhenStopped = true
+        view.addSubview(indicatorView)
+        
+        let scope = ["audio"]
+        indicatorView.startAnimating()
+        VKSdk.wakeUpSession(scope, completeBlock: { state, error in
+            self.indicatorView.stopAnimating()
+            if state == VKAuthorizationState.Authorized {
+                // ready to go
+                let audioRequestDescription = AudioRequestDescription.usersAudioRequestDescription()
+                self.tableViewController.initializeContext(audioRequestDescription)
+            } else if state == VKAuthorizationState.Initialized {
+                // auth needed
+                VKSdk.authorize(scope)
+            } else if state == VKAuthorizationState.Error {
+                self.showMessage("You're now switched to cache-only mode. Pull down to retry.", title: "Failed to authorize")
+                // TODO: Handle appropriately
+            } else if error != nil {
+                fatalError(error.description)
+                // TODO: Handle appropriately
+            }
+        })
+
     }
 
     override func didReceiveMemoryWarning() {
--- VKAudioPlayer/TableViewController.swift
+++ VKAudioPlayer/TableViewController.swift
@@ -106,38 +106,7 @@ class TableViewController: UITableViewController {
         tableView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 100, right: 0)
         refreshControl!.addTarget(self, action: #selector(refresh(_:)), forControlEvents: UIControlEvents.ValueChanged)
         
-//        VKSdk.forceLogout()
-//        return
-        
         definesPresentationContext = false
-        let sdkInstance = VKSdk.initializeWithAppId(appID)
-        sdkInstance.uiDelegate = self
-        sdkInstance.registerDelegate(self)
-        
-        indicatorView.center = view.center
-        indicatorView.activityIndicatorViewStyle = .Gray
-        indicatorView.hidesWhenStopped = true
-        view.addSubview(indicatorView)
-        
-        let scope = ["audio"]
-        indicatorView.startAnimating()
-        VKSdk.wakeUpSession(scope, completeBlock: { state, error in
-            self.indicatorView.stopAnimating()
-            if state == VKAuthorizationState.Authorized {
-                // ready to go
-                let audioRequestDescription = AudioRequestDescription.usersAudioRequestDescription()
-                self.initializeContext(audioRequestDescription)
-            } else if state == VKAuthorizationState.Initialized {
-                // auth needed
-                VKSdk.authorize(scope)
-            } else if state == VKAuthorizationState.Error {
-                self.showMessage("You're now switched to cache-only mode. Pull down to retry.", title: "Failed to authorize")
-                // TODO: Handle appropriately
-            } else if error != nil {
-                fatalError(error.description)
-                // TODO: Handle appropriately
-            }
-        })
         
         searchController.searchResultsUpdater = self
         searchController.searchBar.delegate = self
