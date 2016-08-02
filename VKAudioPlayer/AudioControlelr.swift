96a173c600c32b44f4c2d4123187df535e43fbba
Nikita Belousov <nikita@nikitabs-macbook-pro.local>
2016-08-02 16:01:15 +0300

    Minor changes to AudioContorller

--- VKAudioPlayer/AudioControlelr.swift
+++ VKAudioPlayer/AudioControlelr.swift
@@ -28,23 +28,21 @@ class AudioController {
     private(set) var indexOfCurrentAudioItem: Int?
     private(set) var audioContext: AudioContext?
     private(set) var audioContextSection: AudioContextSection?
+    // means that currentAudioItem is played fully
     private(set) var playedToEnd: Bool = false
     
+    var repeatMode: AudioControllerRepeatMode = .All
+    
+    var paused: Bool {
+        return !((player.rate != 0) && (player.error == nil))
+    }
+    
+    // MARK: Helpers
     
     var currentAudioItem: AudioItem? {
         return audioItemForAudioContext(audioContext, audioContextSection: audioContextSection, index: indexOfCurrentAudioItem)
     }
-    
-    var _repeatMode: AudioControllerRepeatMode = .All
-    var repeatMode: AudioControllerRepeatMode {
-        get {
-            return _repeatMode
-        }
-        set {
-            _repeatMode = newValue
-        }
-    }
-    
+
     func audioItemForAudioContext(audioContext: AudioContext?, audioContextSection: AudioContextSection?, index: Int?) -> AudioItem? {
         if audioContextSection == nil || index == nil || audioContext == nil || index! < 0{
             return nil
@@ -63,45 +61,47 @@ class AudioController {
         return nil
     }
     
+    // MARK: Control
+    
     func playAudioItemFromContext(audioContext: AudioContext?, audioContextSection: AudioContextSection?, index: Int?) {
         
+        // if can derive audioItem
         if let audioItem = audioItemForAudioContext(audioContext, audioContextSection: audioContextSection, index: index) {
             
+            // if not cached and removed by owner -- we can't play it
             if audioItem.url == nil && audioItem.downloadStatus != AudioItemDownloadStatusCached {
                 return
             }
             
+            // reinit controller
             playedToEnd = false
-            
             self.audioContext = audioContext
             self.audioContextSection = audioContextSection
             self.indexOfCurrentAudioItem = index
             
-            // --------------------------------------------
-
+            // post notification that we are going to play an audioItem
             let notification = NSNotification(name: AudioContorllerWillStartPlayingAudioItemNotification, object: nil, userInfo: [
                 "audioItem": audioItem
                 ])
             NSNotificationCenter.defaultCenter().postNotification(notification)
             
+            // get playerItem
             CacheController.sharedCacheController.playerItemForAudioItem(audioItem, completionHandler: { playerItem, cached in
                 
+                // play from main thread
                 dispatch_async(dispatch_get_main_queue(), {
                     self.player.replaceCurrentItemWithPlayerItem(playerItem)
                     self.player.seekToTime(CMTime(seconds: 0, preferredTimescale: 1))
                     self.player.play()
                 })
                 
+                // post notification that we are started to play
                 let notification = NSNotification(name: AudioControllerDidStartPlayingAudioItemNotification, object: nil, userInfo: [
                     "audioItem": playerItem.audioItem
                     ])
                 NSNotificationCenter.defaultCenter().postNotification(notification)
             })
         }
-    }
-    
-    var paused: Bool {
-        return !((player.rate != 0) && (player.error == nil))
     }
     
     func resume() {
@@ -132,6 +132,8 @@ class AudioController {
         if indexOfCurrentAudioItem == nil || audioContextSection == nil || audioContext == nil {
             return
         }
+        
+        // play next if exists, else play first
         if audioItemForAudioContext(audioContext, audioContextSection: audioContextSection, index: indexOfCurrentAudioItem! + 1) != nil {
             indexOfCurrentAudioItem! += 1
         } else {
@@ -144,6 +146,15 @@ class AudioController {
         if indexOfCurrentAudioItem == nil || audioContextSection == nil || audioContext == nil {
             return
         }
+        
+        // replay if track is being played for more then 3 sec
+        let currentTime = player.currentItem?.currentTime()
+        if currentTime?.seconds > 3.0 {
+            replay()
+            return
+        }
+        
+        // play previuos if it exists, else play last
         if audioItemForAudioContext(audioContext, audioContextSection: audioContextSection, index: indexOfCurrentAudioItem! - 1) != nil {
             indexOfCurrentAudioItem! -= 1
         } else {
@@ -160,7 +171,7 @@ class AudioController {
         playAudioItemFromContext(audioContext, audioContextSection: audioContextSection, index: indexOfCurrentAudioItem)
     }
     
-    // MARK: Notifications handling
+    // MARK: Notification handling
     
     @objc private func playerItemDidPlayToEndNotificationHandler(notification: NSNotification) {
         
@@ -188,9 +199,7 @@ class AudioController {
     // MARK:
     
     private init() {
-        
         NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(playerItemDidPlayToEndNotificationHandler), name: AVPlayerItemDidPlayToEndTimeNotification, object: nil)
-        
     }
     
 }
